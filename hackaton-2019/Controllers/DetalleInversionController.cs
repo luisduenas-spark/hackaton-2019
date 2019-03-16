b59874981a7d8076fce0ffd486f35fb486341e6d
luisduenas <luis.duenas@sparktechs.com>
2019-03-15 17:18:21 -0700

    detail invest

---
+++ hackaton-2019/Controllers/DetalleInversionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hackaton_2019.Controllers
+{
+    public class DetalleInversionController : Controller
+    {
+        // GET: DetalleInversion
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
