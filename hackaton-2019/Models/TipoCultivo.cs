3fcc7e8823fe7d8fa6ccb828d5fde03292fac670
Missael Padilla <missael.padilla@sparktechs.com>
2019-03-15 19:15:49 -0700

    catalogo tipos de cultivo

---
+++ hackaton-2019/Models/TipoCultivo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace hackaton_2019.Models
+{
+    public class TipoCultivo
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
